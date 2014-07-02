ea5d97438b3d747a42b16e0f324fc893a035a82c
WillRock19 <williamporto_14@hotmail.com>
2014-07-02 16:12:47 -0300

    Ambiente para testes acaba de ser implementado

---
+++ Bandeira.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Bandeira.Test
+{
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+            var a = 1 + 2;
+
+            a.Should().Be(2);
+
+        }
+    }
+}
