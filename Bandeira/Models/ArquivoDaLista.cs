93d63026ccdcebe0aca32c8bddfe374bc4e7cce4
WillRock19 <williamporto_14@hotmail.com>
2014-07-03 17:04:41 -0300

    Criando Navegacao

---
+++ Bandeira/Models/ArquivoDaLista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bandeira.Models
+{
+    public class ArquivoDaLista
+    {
+        public  string nome     { get; set; }
+        public  string conteudo { get;  set; }
+        public  int proximo { get; set; }
+
+        public ArquivoDaLista(string nome, string cont, int prox)
+        {
+            this.nome     = nome;
+            this.conteudo = cont;
+            this.proximo  = prox; 
+        }
+    }
+}
