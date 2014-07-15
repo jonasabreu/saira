e688237df03f811cc849a48b2828adde2e0f672d
WillRock19 <williamporto_14@hotmail.com>
2014-07-15 18:26:26 -0300

    Novos testes de interacao adicionados

--- Bandeira.Integration.Test/UnitTest1.cs
+++ Bandeira.Integration.Test/UnitTest1.cs
@@ -9,21 +9,83 @@ namespace Bandeira.Integration.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ExceçaoDeURL()
         {
             var driver = TestEnvironment.driver;
-            driver.Navigate().GoToUrl("http://localhost:49892/");
-
+            driver.Navigate().GoToUrl("http://localhost:8080/");
             driver.FindElement(By.Id("URLRepo")).SendKeys("asdf");
-            driver.FindElement(By.Id("DirID")).SendKeys("asdf");
+            driver.FindElement(By.Id("DirID")).SendKeys("C:/TestaArquivosOnline");
+            driver.FindElement(By.Name("btnExecutar")).Click();
+            driver.FindElementByCssSelector("#URLRepo:invalid").Should().NotBeNull();
+        }
 
+        [TestMethod]
+        public void ExceçaoDoID()
+        {
+            var driver = TestEnvironment.driver;
+            driver.Navigate().GoToUrl("http://localhost:8080/");
+            driver.FindElement(By.Id("URLRepo")).SendKeys("https://github.com/jonasabreu/leis-site.git");
+            driver.FindElement(By.Id("DirID")).SendKeys("asdsd");
+            driver.FindElement(By.Name("btnExecutar")).Click();
+            driver.FindElementByClassName("field-validation-error").Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void ExceçaoDaURLeID()
+        {
+            var driver = TestEnvironment.driver;
+                driver.Navigate().GoToUrl("http://localhost:8080/");
+                driver.FindElement(By.Name("btnExecutar")).Click();
+
+            var listaErros = driver.FindElementsByClassName("field-validation-error");
+                listaErros[0].Text.Should().Be("Favor, insira uma URL");
+                listaErros[1].Text.Should().Be("Favor, insira um diretório onde salvar os dados");
+        }
+
+        [TestMethod]
+        public void AcessarDetalhesDeArquivoZero()
+        {
+            var driver = TestEnvironment.driver;
+            driver.Navigate().GoToUrl("http://localhost:8080/");
+            driver.FindElement(By.Id("URLRepo")).SendKeys("https://github.com/jonasabreu/leis-site.git");
+            driver.FindElement(By.Id("DirID")).SendKeys("C:/TestaArquivosOnline");
+            driver.FindElement(By.Name("btnExecutar")).Click();
+            driver.Url.Should().Be("http://localhost:8080/Home/Detalhes/0");
+        }
+
+        [TestMethod]
+        public void NavegarParaPaginaI()
+        {
+            var driver = TestEnvironment.driver;
+            driver.Navigate().GoToUrl("http://localhost:8080/");
+            driver.FindElement(By.Id("URLRepo")).SendKeys("https://github.com/jonasabreu/leis-site.git");
+            driver.FindElement(By.Id("DirID")).SendKeys("C:/TestaArquivosOnline");
             driver.FindElement(By.Name("btnExecutar")).Click();
 
-            driver.Url.Should().Be("asdasdasd");
+            for(int i = 0; i <= 5; i++)
+            {
+                driver.Url.Should().Be("http://localhost:8080/Home/Detalhes/" + i);
+                driver.FindElement(By.CssSelector(".proximo a")).Click();
+            }
+        }
 
+        [TestMethod]
+        public void NavegarParaIEVoltarParaIMenos1()
+        {
+            var driver = TestEnvironment.driver;
+            driver.Navigate().GoToUrl("http://localhost:8080/");
+            driver.FindElement(By.Id("URLRepo")).SendKeys("https://github.com/jonasabreu/leis-site.git");
+            driver.FindElement(By.Id("DirID")).SendKeys("C:/TestaArquivosOnline");
+            driver.FindElement(By.Name("btnExecutar")).Click();
+            int i;
 
-
-
+            for (i = 0; i <= 3; i++)
+            {
+                driver.Url.Should().Be("http://localhost:8080/Home/Detalhes/" + i);
+                driver.FindElement(By.CssSelector(".proximo a")).Click();
+            }
+            driver.FindElement(By.CssSelector(".voltar a")).Click();
+            driver.Url.Should().Be("http://localhost:8080/Home/Detalhes/" + (i - 1));
         }
     }
 }
